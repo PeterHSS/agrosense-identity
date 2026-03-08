0ae74249e226c834b39c14b6e4240dc4a4197797
PeterHSS <pedro.henrique.ismart@gmail.com>
2026-03-08 02:14:01 -0300

    refactor(db): rename Default connection string to Identity
    Renamed the connection string key from "Default" to "Identity" in appsettings.json.
    Updated DependencyInjectionExtension.cs to use the "Identity" connection string when configuring UserDbContext.
    No changes were made to the actual connection string value.
    Ensured consistency between configuration and code regarding connection string naming.

--- Api/DependencyInjectionExtension.cs
+++ Api/DependencyInjectionExtension.cs
@@ -126,7 +126,7 @@ public static class DependencyInjectionExtension
 
     private static IServiceCollection AddContexts(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("Default") ?? throw new InvalidOperationException("ConnectionString not configured.");
+        var connectionString = configuration.GetConnectionString("Identity") ?? throw new InvalidOperationException("ConnectionString not configured.");
 
         services.AddDbContext<UserDbContext>(options => options.UseNpgsql(connectionString));
 
